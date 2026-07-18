0d7281f2284221712b85a39c3aa67ad301632318
Shakya <your_email@gmail.com>
2026-07-18 17:13:35 +0530

    Run browsers in headless mode for GitHub Actions

--- conftest.py
+++ conftest.py
@@ -6,8 +6,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+
 
 @pytest.fixture(params=["chrome", "firefox"])
 def driver(request):
@@ -15,13 +19,25 @@ def driver(request):
     browser = request.param
 
     if browser == "chrome":
+
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
         driver = webdriver.Chrome(
-            service=ChromeService(ChromeDriverManager().install())
+            service=ChromeService(ChromeDriverManager().install()),
+            options=chrome_options
         )
 
     elif browser == "firefox":
+
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument("--headless")
+
         driver = webdriver.Firefox(
-            service=FirefoxService(GeckoDriverManager().install())
+            service=FirefoxService(GeckoDriverManager().install()),
+            options=firefox_options
         )
 
     driver.maximize_window()
@@ -33,14 +49,20 @@ def driver(request):
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
+
     outcome = yield
     report = outcome.get_result()
 
     if report.when == "call" and report.failed:
+
         driver = item.funcargs.get("driver")
 
         if driver:
+
             os.makedirs("screenshots", exist_ok=True)
+
             filename = f"screenshots/{item.name}.png"
+
             driver.save_screenshot(filename)
+
             print(f"Screenshot saved: {filename}")
